92c5156ac8b81b3f2c8efacac260aeb8d2a3e2ab
Andre Jarl <andrejarlaniana@gmail.com>
2025-06-29 09:40:23 +0800

    added z-50 to the addtolist button

--- frontend/src/components/AddToListButton.jsx
+++ frontend/src/components/AddToListButton.jsx
@@ -6,7 +6,7 @@ function AddToListButton() {
     const [buttonClicked, setButtonClicked] = useState(false);
 
   return (
-  <div className="flex flex-row items-center gap-5 mt-5 mb-10">
+  <div className="flex flex-row items-center gap-5 mt-5">
   <button className="bg-red-800 px-4 py-2 lg:px-6 lg:py-3 lg:text-base rounded-3xl font-medium flex flex-row text-xs items-center text-center gap-1">
     <Play /> WATCH NOW
   </button>
@@ -19,7 +19,7 @@ function AddToListButton() {
       <Plus /> ADD LIST
     </button>
     <div
-      className={`absolute ${buttonClicked ? 'flex' : 'hidden'} right-0 top-full mt-2 shadow-2xl border-2 border-white backdrop-blur-xl rounded-lg`}
+      className={`absolute ${buttonClicked ? 'flex' : 'hidden'} right-0 top-full mt-2 z-50 shadow-2xl border-2 border-white backdrop-blur-xl rounded-lg`}
     >
       <ul className="flex flex-col gap-5 p-4 text-white text-sm">
         <li className="hover:text-red-600 cursor-pointer">Favorites</li>
