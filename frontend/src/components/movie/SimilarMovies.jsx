003bc8e47b2cb50cad5e0e6c5e95fbe6c9f10002
Andre Jarl <andrejarlaniana@gmail.com>
2025-07-16 12:16:10 +0800

    adjusted similar movie width

--- frontend/src/components/movie/SimilarMovies.jsx
+++ frontend/src/components/movie/SimilarMovies.jsx
@@ -40,7 +40,7 @@ function SimilarMovies({id}) {
   <div className="grid lg:grid-cols-5 grid-cols-2 md:grid-cols-3 gap-10 justify-items-center lg:mx-0  mx-5">
     {movieRecom.slice(0,5).map((movie, index) => (
        <Link to={`/movie-details/${movie.id}`}><div key={index} className='flex flex-col text-white gap-2'  onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-            <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}  className="lg:h-[300px] z-50 hover:scale-110 mt-5 transition-transform duration-300 w-[200px] object-cover rounded-lg shadow-md"/>
+            <img  src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title}  className="lg:h-[300px] z-50 hover:scale-110 mt-5 transition-transform duration-300 w-[180px] object-cover rounded-lg shadow-md"/>
              
             <p className='text-white px-1 font-bold w-[180px] truncate'>{movie.title}</p>
             <div className='flex flex-row justify-between mx-2'> 
