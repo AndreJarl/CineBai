6a6694fc265a067067f5509eeef52bc37fa8c267
Andre Jarl <andrejarlaniana@gmail.com>
2025-11-03 09:02:53 +0800

    added optional auth to fix 401 error from protectRoutes when sending link

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import{ login, signup, logout, authCheck, forgotPassword, verifyResetToken, resetPassword } from "../controllers/auth.controllers.js"
 import { protectRoutes } from '../middlewares/proctectRoutes.js';
+import { optionalAuth } from '../middlewares/optionalAuth.js';
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.get("/authCheck", protectRoutes, authCheck);
+router.get("/authCheck", optionalAuth, authCheck);
 router.post('/forgot-password', forgotPassword);
 router.post('/verify-reset-token', verifyResetToken);
 router.post('/reset-password', resetPassword);
